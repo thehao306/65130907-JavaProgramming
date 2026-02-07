9dad42a79ed89e541c06bbdac6f267cf754f85e4
thehao306 <hao.tt.65cntt@ntu.edu.vn>
2026-02-07 20:47:25 +0700

    Thêm bài tập 2

---
+++ Bai_tap2/src/Bai_tap2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Bai_tap2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+	}
+
+}
