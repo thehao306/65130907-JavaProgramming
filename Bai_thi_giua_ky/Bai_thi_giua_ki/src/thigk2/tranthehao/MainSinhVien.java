7a9504ab3cd5c3629fd757681363ae006240bd88
thehao306 <hao.tt.65cntt@ntu.edu.vn>
2026-04-23 07:51:23 +0700

    COMMIT lan thu 2

---
+++ Bai_thi_giua_ky/Bai_thi_giua_ki/src/thigk2/tranthehao/MainSinhVien.java
@@ -0,0 +1,54 @@
+package thigk2.tranthehao;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainSinhVien {
+    public static void main(String[] args) {
+        ArrayList<SinhVien> ds = new ArrayList<>();
+
+        // Hard-code 3 sinh viên
+        ds.add(new SinhVien("SV01", "TranTheHao", 2005, 8.0));
+        ds.add(new SinhVien("SV02", "Tran Van B", 2005, 7.5));
+        ds.add(new SinhVien("SV03", "Le Van C", 2005, 9.0));
+
+        System.out.println("Danh sách ban đầu:");
+        for (SinhVien sv : ds) {
+            sv.xuat();
+        }
+
+        // Nhập thêm sinh viên
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nNhập sinh viên mới:");
+
+        System.out.print("Mã SV: ");
+        String ma = sc.nextLine();
+
+        System.out.print("Họ tên: ");
+        String ten = sc.nextLine();
+
+        System.out.print("Năm sinh: ");
+        int ns = sc.nextInt();
+
+        System.out.print("Điểm TB: ");
+        double diem = sc.nextDouble();
+
+        ds.add(new SinhVien(ma, ten, ns, diem));
+
+        // In lại danh sách
+        System.out.println("\nDanh sách sau khi thêm:");
+        for (SinhVien sv : ds) {
+            sv.xuat();
+        }
+
+        // Sinh viên > 20 tuổi
+        System.out.println("\nSinh viên > 20 tuổi:");
+        for (SinhVien sv : ds) {
+            if (sv.getTuoi() > 20) {
+                sv.xuat();
+            }
+        }
+
+        sc.close();
+    }
+}
